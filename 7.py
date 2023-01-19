fac4501319a2b640e37b8e59ac7513075b564f6b
Aiya <moldakasyma@mail.ru>
2023-01-19 17:15:46 +0600

    first commit

---
+++ 7.py
@@ -0,0 +1,25 @@
+x="Hello World!"
+y=20
+z=20.5
+k=1j
+a=["apple","banana","cherry"]
+b=("apple","banana","cherry")
+c=range(6)
+d={"name" : "John","age":36}
+m= b"Hello"
+n = bytearray(5)
+o= memoryview(bytes(5))	
+p=None
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(k))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(m))
+print(type(n))
+print(type(o))
+print(type(p))
+
