fe53cf31720deb1581a48f5ad31132870289547e
moldakasyma <117009050+moldakasyma@users.noreply.github.com>
2023-02-19 21:16:39 +0600

    First commit

---
+++ tsis4/math/2.py
@@ -0,0 +1,6 @@
+import math
+h=float(input())
+a=float(input())
+b=float(input())
+s=(a+b)/2*h
+print(float(s))
---
+++ tsis4/math/3.py
@@ -0,0 +1,6 @@
+import math
+n=int(input())
+l=int(input())
+t=math.tan((math.pi)/n)
+S=0.25*l*l*n/t
+print(int(S))
